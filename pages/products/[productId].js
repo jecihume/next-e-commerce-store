3622acdfad72c5748e266c6294ddf0d86e1f4fba
JeciHume <jecihume@gmail.com>
2021-10-13 21:37:08 +0200

    Implement quantity counter for cookie

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -7,23 +7,44 @@ const product = (props) => {
   // this is only used, when I am grabbing information from the front end through the URl:
   // const router = useRouter();
   // const { product } = router.query;
-
+  console.log(props);
   // if (typeof window !== 'undefined') {
   //   console.log(window.localStorage);
   // }
-
+  // TODO: get the cookie
   const clickHandler = () => {
+    // this console.log is showing the whole objects of the products
     console.log(props.dragonEgg.id);
 
     //case 1: empty cart or cookie undefined -> create cookie
-
     // Cookies.set('cart', JSON.stringify([props.dragonEgg.id]));
     const oldCookie = getCookie('cart');
     // add more stuff to the virtual bag:
     if (oldCookie === undefined) {
       setCookie('cart', [{ id: props.dragonEgg.id, qty: 1 }]);
-    } else {
+    } else if (
+      !oldCookie.some((cookieEgg) => {
+        return (
+          cookieEgg.id === props.dragonEgg.id
+        ); /* this is a boolean, that's why I am returning it! */
+      })
+      /* if the cookie exists, but doesn't contain an item with the id of the Egg, add up */
+    ) {
       const newCookie = [...oldCookie, { id: props.dragonEgg.id, qty: 1 }];
+      setCookie('cart', newCookie);
+    } else {
+      /* cookie contains already an Egg with that Id*/
+      /* [{id:2, qty:1}, {id:5, qty: 2}] this is representing the oldCookie; cookieEgg is the object inside of the array of eggs! */
+      const newCookie = oldCookie.map((cookieEgg) => {
+        if (cookieEgg.id === props.dragonEgg.id) {
+          const newCookieEgg = cookieEgg;
+          newCookieEgg.qty = newCookieEgg.qty + 1;
+          return newCookieEgg;
+        } else {
+          return cookieEgg;
+        }
+      });
+
       setCookie('cart', newCookie);
     }
     // filter to add quantity by one!
@@ -84,5 +105,16 @@ export async function getServerSideProps(context) {
     props: { dragonEgg },
   };
 }
+
+// const eggsInCart = [{dragonEgg}];
+
+//   export default function CartItems() {
+//     const howMany = eggsInCart.reduce((acc, item) => {
+//     const newAmount = acc + item.qty;
+//     return newAmount;
+//   }, 0);
+
+//   return <div>CartItems: {JSON.stringify(howMany)}</div>;
+//   }
 
 export default product;
